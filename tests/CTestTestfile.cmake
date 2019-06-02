be8bdfee880d268cb8ae8c32d8fa32b1c3e5889e
Roman Stanchak <rstanchak@gmail.com>
2019-06-02 00:25:55 -0400

    refactor and clean up code; add test and coverage hooks

---
+++ tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/roman/hackerrank/astar/tests
+# Build directory: /home/roman/hackerrank/astar/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example_test "../build/astar-cpp" "<" "input1.txt")
